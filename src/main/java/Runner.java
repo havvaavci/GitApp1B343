a83c92a16c14631795e4744819aa4f6758d4c8c6
Havva AVCI <havvabuyukyalcin@gmail.com>
2025-09-28 17:13:45 +0200

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,8 @@ public class Runner {
         //version 1
         System.out.println("Hello GIT");
         System.out.println("git add . : Staging area'ya alir");
+
+        //version 2
+        System.out.println("git commit -m\"message\" ile versiyon olusturulur");
     }
 }
